9951f43391c9d8f7d091660bd69dd7d8c675e45d
Barannik <obaranni@student.unit.ua>
2018-07-29 18:46:09 +0300

    working vercion

--- src/print_string.c
+++ src/print_string.c
@@ -1,7 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_string.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obaranni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/29 18:43:47 by obaranni          #+#    #+#             */
+/*   Updated: 2018/07/29 18:45:59 by obaranni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../headers/ft_printf.h"
 
+void		preparing_strings_shame(t_p *p)
+{
+	int new_prec;
+	int i;
 
-int 		print_any_string(t_p *p)
+	i = 0;
+	new_prec = 0;
+	if (p->data_uint)
+		which_string(p);
+	if (!p->precision && p->prec_finded)
+		p->precision = 0;
+	else if (!p->precision || DLEN <= p->precision)
+		p->precision = p->data_len;
+	else if (DLEN > p->precision)
+	{
+		while (p->data_uint_copy[i])
+		{
+			if (count_uni_char_len(p->data_uint_copy[i])
+					+ new_prec > p->precision)
+				break ;
+			else
+				new_prec += count_uni_char_len(p->data_uint_copy[i]);
+			i++;
+		}
+		p->precision = new_prec;
+	}
+}
+
+int			preparing_strings(t_p *p)
+{
+	int		res;
+
+	flags_priority(p);
+	preparing_strings_shame(p);
+	if (p->width && p->precision)
+		p->width -= p->precision;
+	if (p->width < 0)
+		p->width = 0;
+	if (p->data_uint_copy)
+		res = p->width + p->precision;
+	else
+		res = p->width;
+	if (p->minus)
+	{
+		if (p->data_uint_copy)
+			printing_any_str(p);
+		print_width_str(p);
+	}
+	else
+	{
+		print_width_str(p);
+		if (p->data_uint_copy)
+			printing_any_str(p);
+	}
+	return (res);
+}
+
+int			print_any_string(t_p *p)
 {
 	if (!p->data_uint && !p->width)
 	{
@@ -9,50 +77,5 @@ int 		print_any_string(t_p *p)
 		return ((int) ft_strlen("(null)"));
 	}
 	else
-    {
-        int res;
-        flags_priority(p);
-        if (p->data_uint)
-            which_string(p);
-        if (!p->precision && p->prec_finded)
-            p->precision = 0;
-        else if (!p->precision || DLEN <= p->precision)
-            p->precision = p->data_len;
-        else if (DLEN > p->precision)
-        {
-            int new_prec = 0;
-            int i = 0;
-
-            while (p->data_uint_copy[i])
-            {
-                if (count_uni_char_len(p->data_uint_copy[i]) + new_prec > p->precision)
-                    break ;
-                else
-                    new_prec += count_uni_char_len(p->data_uint_copy[i]);
-                i++;
-            }
-            p->precision = new_prec;
-        }
-        if (p->width && p->precision)
-            p->width -= p->precision;
-		if (p->width < 0)
-			p->width = 0;
-		if (p->data_uint_copy)
-			res = p->width + p->precision;
-		else
-			res = p->width;
-		if (p->minus)
-		{
-            if (p->data_uint_copy)
-			    printing_any_str(p);
-			print_width_str(p);
-		}
-		else
-		{
-			print_width_str(p);
-            if (p->data_uint_copy)
-                printing_any_str(p);
-		}
-		return (res);
-	}
+		return (preparing_strings(p));
 }
