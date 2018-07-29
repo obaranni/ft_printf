afdabbec846636c5c0786fdd48235dad6e73ac63
Barannik <obaranni@student.unit.ua>
2018-07-29 17:46:08 +0300

    fdlasjsldjf

--- src/printing_unicode.c
+++ src/printing_unicode.c
@@ -16,11 +16,13 @@ void				printing_unicode(unsigned int *u)
 {
 	int				i;
 	unsigned int	f;
+	unsigned int	box;
 
 	i = 0;
+	box = *u;
 	while (i < 4)
 	{
-		f = ((*u << (i * 8)) >> 24);
+		f = ((box << (i * 8)) >> 24);
 		if (f)
 			write(1, &f, 1);
 		i++;
